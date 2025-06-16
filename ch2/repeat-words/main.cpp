5832ce2537b5764a8c6dc2422522fe5b01a381f8
Gnu Doc <20248043+gnudoc@users.noreply.github.com>
2025-06-16 12:33:12 +0100

    repeat count

--- ch2/repeat-words/main.cpp
+++ ch2/repeat-words/main.cpp
@@ -2,11 +2,13 @@
 #include "PPPheaders.h"
 
 int main() {
+    int num_words = 0;
     string previous;
     string current;
     while (cin>>current) {
+        ++num_words;
         if (previous == current)
-            cout << "repeated word: " << current << '\n';
+            cout << "word number " << num_words << " repeated word: " << current << '\n';
         previous = current;
     }
 }
