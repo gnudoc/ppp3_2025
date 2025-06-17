e89fa6a0e3ad013af2ffed315c5cef33ce14787e
Gnu Doc <20248043+gnudoc@users.noreply.github.com>
2025-06-17 10:27:31 +0100

    start ch2 drill

---
+++ ch2/drill/PPPheaders.h
@@ -0,0 +1,32 @@
+// PPPheaders.h
+
+#include<iostream>
+#include <sstream>
+#include<string>
+#include<vector>
+#include<span>
+#include<stdexcept>
+#include<random>
+
+#include<stdint.h>
+#include<list>
+#include <map>
+#include<unordered_map>
+#include <set>
+#include<memory>
+#include <algorithm>
+
+using namespace std;
+
+
+#define PPP_EXPORT
+#include "PPP_support.h"
+
+using namespace PPP;
+
+
+// disgusting macro hack to get a range checking:
+#define vector Checked_vector
+#define string Checked_string
+#define span Checked_span
+
